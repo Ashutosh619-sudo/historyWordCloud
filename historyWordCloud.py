584f214d0ad9bcb0e4de9e29d38121982a9a708f
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2020-01-16 19:26:31 +0530

    created main file

---
+++ historyWordCloud.py
@@ -0,0 +1,32 @@
+import browserhistory as bh
+import pandas as pd
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+#print(bh.get_browserhistory())
+#bh.write_browserhistory_csv()
+
+
+history = pd.read_csv("firefox_history.csv")
+history = history[~history["browserhistory · PyPI"].str.contains("New Tab", na=False)]
+
+plt.figure(figsize=(15,10))
+history = history.dropna()
+
+history.isna().sum()
+
+text = "".join(hist for hist in history["browserhistory · PyPI"])
+print(len(text))
+
+#remove the words which you think are not relevant
+stopwords = ["Google", "YouTube","Graphic","Hill University","Hill","University"]
+
+STOPWORDS.update(stopwords)
+
+wordcloud = WordCloud(stopwords=STOPWORDS,background_color="white").generate(text)
+wordcloud.to_file("third.png")
+
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
